6957f076d1a08d736234627af2fa57afbf279d4a
theodosis777 <theodosis777@gmail.com>
2021-08-08 19:38:53 +0300

    q

--- React/Project1/my-first-app/src/App.js
+++ React/Project1/my-first-app/src/App.js
@@ -1,17 +1,11 @@
-
+import Todo from './components/Todo';
 
 function App() {
   return (<div>
   <h1>My Todos</h1>
-  <div className='card'>
-  <div>
-    <h2>Title</h2>
-    <div>
-    <button>Delete</button>
-    </div>
+  <Todo />
   </div>
-  </div>
-  </div>
+
 );
 }
 
