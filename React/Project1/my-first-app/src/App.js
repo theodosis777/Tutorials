5bfea1286ed27cdbfc723d2301621ec5bd500848
theodosis777 <theodosis777@gmail.com>
2021-08-08 21:13:03 +0300

    as

--- React/Project1/my-first-app/src/App.js
+++ React/Project1/my-first-app/src/App.js
@@ -1,12 +1,17 @@
-import Todo from './components/Todo';
+import Todo from "./components/Todo";
+
 
 function App() {
-  return (<div>
-  <h1>My Todos</h1>
-  <Todo />
-  </div>
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Expore the full react course" />
 
-);
+    
+    </div>
+  );
 }
 
 export default App;
