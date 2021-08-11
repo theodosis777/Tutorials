c9073f4ae6c119da0d61dbe22efef09853f0bf48
theodosis777 <theodosis777@gmail.com>
2021-08-11 10:33:18 +0300

    project 2

---
+++ React/Project2/my-first-app/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar icon='fab fa-github' />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
