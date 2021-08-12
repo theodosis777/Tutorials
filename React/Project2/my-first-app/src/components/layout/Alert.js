3996af0929da76f503ad189e3319735ac79a2632
theodosis777 <theodosis777@gmail.com>
2021-08-12 08:06:09 +0300

    1208-1

---
+++ React/Project2/my-first-app/src/components/layout/Alert.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Alert = ({ alert }) => {
+  return (
+    alert !== null && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className='i.fas.fa-info-circle' />
+        {alert.msg}
+      </div>
+    )
+  );
+};
+
+export default Alert;
