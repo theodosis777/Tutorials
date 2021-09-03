cda6106b6be26c7534e5817ef2050c1b61f1765c
theodosis777 <theodosis777@gmail.com>
2021-09-03 16:00:35 +0300

    a

--- React/CONTACTS/client/src/components/pages/About.js
+++ React/CONTACTS/client/src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div>
       <h1>About This App</h1>
       <p className='bg-dark p'>
-        <strong> Version:</strong>1.0.0
+        <strong> Created by:</strong>Theodosis Demetriou
       </p>
       <p className='my-1'>
         This is a full stack React app for keeping contacts
